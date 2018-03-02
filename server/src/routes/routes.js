e6d2e8852ae36364be82661320e660259567287f
lvlam <liem.lam@gmail.com>
2018-03-02 08:44:04 -0500

    added jwt authentication service

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,6 +1,13 @@
 const MachinesController = require('../controllers/MachinesController')
+const AuthenticationController = require('../controllers/AuthenticationController')
+const AuthenticationControllerPolicy = require('../policies/AuthenticationControllerPolicy')
 
 module.exports = (app) => {
+  app.post('/register',
+    AuthenticationControllerPolicy.register,
+    AuthenticationController.register)
+  app.post('/login',
+    AuthenticationController.login)
   app.get('/machines',
     MachinesController.index),
   app.post('/machines',
